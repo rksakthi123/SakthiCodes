546dc7c3f387b5ff9cc1ae893b974bbe689163a9
K <sakaliyaperumal@ebay.com>
2017-08-12 15:41:44 +0530

    testng

--- javacodes/src/main/java/com/sakthi/project/javacodes/Palindrom.java
+++ javacodes/src/main/java/com/sakthi/project/javacodes/Palindrom.java
@@ -6,12 +6,13 @@ public class Palindrom {
 
 	public static void main(String[] args) {
 		Palindrom obj=new Palindrom();
-		obj.test("baab");
+		//obj.test("baab");
 
 	}
 	
 	@Test(groups="test123")
-	public void test(String str){
+	public void test(){
+		String str="baab";
 		boolean status=true;
 		for(int i=0,j=str.length()-1;i<str.length()/2;i++,j--){
 			if(str.charAt(i)!=str.charAt(j)){
