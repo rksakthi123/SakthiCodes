847a65c63db769aa1a299f8cb08fa260be135bc1
K <sakaliyaperumal@ebay.com>
2017-08-12 15:24:40 +0530

    files

---
+++ javacodes/src/main/java/com/sakthi/project/javacodes/Palindrom.java
@@ -0,0 +1,29 @@
+package com.sakthi.project.javacodes;
+
+import org.testng.annotations.Test;
+
+public class Palindrom {
+
+	public static void main(String[] args) {
+		Palindrom obj=new Palindrom();
+		obj.test("baab");
+
+	}
+	
+	@Test(groups="test123")
+	public void test(String str){
+		boolean status=true;
+		for(int i=0,j=str.length()-1;i<str.length()/2;i++,j--){
+			if(str.charAt(i)!=str.charAt(j)){
+				status=false;
+			}
+		}
+		if(status){
+			System.out.println("Palindrom");
+		}
+		else{
+			System.out.println("Not a palindrom");
+		}
+	}
+
+}
